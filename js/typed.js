92f21a1ce4bb0896c4cc70c5743b2dac2a2d09f3
Tom Eloe <tomeloe@gmail.com>
2018-10-16 14:07:22 -0700

    added imgs/ typed.js

---
+++ js/typed.js
@@ -0,0 +1,11 @@
+$(function(){
+   
+   var typed = new Typed('#name', {
+    strings: ["Hi, I'm Tom"],
+    typeSpeed: 40,
+    startDelay: 500,
+    showCursor: true,
+    cursorChar: '_'
+  });
+
+});
